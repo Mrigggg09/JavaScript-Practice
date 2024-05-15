d0d979828e59c43fc1efe994ac916b189f0289a4
Mriganshu <107197367+Mrigggg09@users.noreply.github.com>
2024-05-15 16:04:23 +0000

    updating 01_variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,19 @@
-const
+const accountId = 12345
+let accountEmail = "mriganshu@gmail.com"
+var accountPassword = "1223344"
+accountCity = "Jamshedpur"
+let accountState;
+
+// accountId = 54321 // not allowed
+
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+accountEmail = "mp@gmail.com"
+accountPassword = "5544332211"
+accountCity = "Haldia"
+
+console.log(accountId)
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
