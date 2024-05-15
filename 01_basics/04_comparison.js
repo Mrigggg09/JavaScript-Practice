d5d8f86b43cd235ae7286631d7ef948c548004be
Mriganshu <107197367+Mrigggg09@users.noreply.github.com>
2024-05-15 17:56:56 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 3)
+// console.log(2 < 3)
+// console.log(2 == 3)
+// console.log(2 >= 3)
+// console.log(2 != 3)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+//strict check checks for datatype too so best for comparision equality
+
+console.log("2" === 2);
