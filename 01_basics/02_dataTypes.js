9c9e916e900a9d71139208ac600cf0d0e1b69fd7
Mriganshu <107197367+Mrigggg09@users.noreply.github.com>
2024-05-15 16:38:10 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict" // treats all JS codes as newer version
+
+// alert(3+3) // We are using nodejs not browser so this won't work
+
+//; is optional
+
+//Readability should be high (use proper spacing)
+
+//let and var are the same thing(let is used/preffered)
+
+console.log(3+3)
+console.log("Mriganshu")
+
+
+let name = "Sunny"
+let age = 21
+let loggedIn = true
+
+/*
+DataTypes: 
+    number => 2 to the power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value (nothing or no value)
+    undefined => to value added to a declared variable
+    symbol => unique
+    object
+*/
+
+console.log(typeof "Hello") //string
+console.log(typeof age) //number
+console.log(typeof null) //object
