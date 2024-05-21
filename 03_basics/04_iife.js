c268675ad159f1e3ac59fa5664b0eb1615741941
Mriganshu <107197367+Mrigggg09@users.noreply.github.com>
2024-05-21 10:28:08 +0000

    iife update

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,7 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-
+//IIFE avoids global scope pollution(for var data)
+//IIFE are function which get executed immediately
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
